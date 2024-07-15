7f556a4f8a7df3dd7c7ba3f535f985372583bfc8
liangjian <liangjian@oliveche.com>
2024-07-15 17:31:01 +0800

    merge from fotric: bugfix: ueditor内添加表格,在表格内鼠标*有时*无法进行点击和选择，会自动回到开头位置。

--- server/web/store.js
+++ server/web/store.js
@@ -12,16 +12,20 @@ function enhanceUeditor(jp)
 	// var opt = WUI.getOptions(jp, defOpt);
 	var dfd = $.Deferred();
 
-	var editor = new UE.ui.Editor();
-	editor.render(jp[0]);
-	editor.addListener('ready',function(){
-		dfd.resolve();
-	})
-
+	var editor = null;
 	jdlg.on("show", onShow)
 		.on("validate", onValidate);
 
 	function onShow(ev) {
+		// bugfix: ueditor内添加表格,在表格内鼠标*有时*无法进行点击和选择，会自动回到开头位置。(ueditor.all.min.js)
+		// 注意：必须在onShow中做render, 否则iframe.src中的代码很可能会被执行两次, 导致`N.addListener("mouseup", w)` 会执行两次导致鼠标选择异常
+		if (editor == null) {
+			editor = new UE.ui.Editor();
+			editor.render(jp[0]);
+			editor.addListener('ready',function(){
+				dfd.resolve();
+			})
+		}
 		dfd.then(function () {
 			editor.setContent(jp.val());
 		});
